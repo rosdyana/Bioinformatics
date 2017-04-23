2a9dfbc1aa92cef9403f75356dcc9eaacc98ac36
Rosdyana Kusuma <rosdyana.kusuma@gmail.com>
2017-04-23 22:39:22 +0800

    - added some menus ;)

--- Tools/ARFF2LIBSVM/server.R
+++ Tools/ARFF2LIBSVM/server.R
@@ -1,1 +1,14 @@
-server <- function(input, output) {}
+server <- function(input, output) {
+  observeEvent(input$about, {
+    showModal(modalDialog(
+      title = span(tagList(icon("info-circle"), "About")),
+      tags$div(
+        HTML(
+          "<img src='https://avatars1.githubusercontent.com/u/4516635?v=3&s=460' width=150><br/><br/>",
+          "<p>Developer : Rosdyana Kusuma</br>Email : <a href=mailto:rosdyana.kusuma@gmail.com>rosdyana.kusuma@gmail.com</a></br>linkedin : <a href='https://www.linkedin.com/in/rosdyanakusuma/' target=blank>Open me</a></p>"
+        )
+      ),
+      easyClose = TRUE
+    ))
+  })
+}
--- Tools/ARFF2LIBSVM/ui.R
+++ Tools/ARFF2LIBSVM/ui.R
@@ -21,6 +21,11 @@ ui <- dashboardPage(
         'LIBSVM 2 ARFF',
         tabName = 'libsvm2arff',
         icon = icon('backward')
+      ),
+      menuSubItem(
+        'LIBSVM READER',
+        tabName = 'libsvmreader',
+        icon = icon('eye')
       )
     ),
     actionButton("about", "About", icon = icon("info-circle")),
@@ -31,5 +36,53 @@ ui <- dashboardPage(
       onclick = "window.open('https://github.com/rosdyana/Bioinformatics', '_blank')"
     )
   )),
-  dashboardBody()
+  dashboardBody(
+    tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", href = "https://fonts.googleapis.com/css?family=Oregano")
+    ),
+    tags$head(tags$style(
+      HTML(
+        '
+        .main-header .logo {
+        font-family: "Oregano", cursive;
+        font-weight: bold;
+        font-size: 24px;
+        }
+        '
+      )
+      )),
+    tabItems(
+      # First tab content
+      tabItem(tabName = "arff2libsvm",
+              fluidPage(
+                box(
+                  title =   tagList(shiny::icon("forward") , "ARFF 2 LIBSVM"),
+                  width = NULL,
+                  status = "primary",
+                  solidHeader = TRUE,
+                  collapsible = TRUE
+                )
+              )),
+      tabItem(tabName = "libsvm2arff",
+              fluidPage(
+                box(
+                  title =   tagList(shiny::icon("backward") , "LIBSVM 2 ARFF"),
+                  width = NULL,
+                  status = "primary",
+                  solidHeader = TRUE,
+                  collapsible = TRUE
+                )
+              )),
+      tabItem(tabName = "libsvmreader",
+              fluidPage(
+                box(
+                  title =   tagList(shiny::icon("eye") , "LIBSVM READER"),
+                  width = NULL,
+                  status = "primary",
+                  solidHeader = TRUE,
+                  collapsible = TRUE
+                )
+              ))
+      )
+  )
 )
